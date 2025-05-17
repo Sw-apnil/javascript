c9840fd546d80fd911bdcf8b235eb9f6d1c1068e
Sw-apnil <148454649+Sw-apnil@users.noreply.github.com>
2025-05-17 05:48:33 +0000

    variables

---
+++ 01_basics/let_const_var.js
@@ -0,0 +1,12 @@
+const accountId=1444
+let accountEmail="hello@gmail.com"
+ var accountPaswrd="123445"
+ let accountState;
+ // const variable can not be changed
+ // accountId=4555 now allowed
+ /* do not use var because it has scope issues
+ */ 
+ accountEmail="hchhh@.com"
+ accountPaswrd="567456"
+ console.log(accountId)
+ console.table([accountId,accountEmail,accountPaswrd,accountState])
