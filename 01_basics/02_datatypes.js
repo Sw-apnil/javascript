22743a48fa8d00b59079cbdb1d0d5b5e964e5cc1
Sw-apnil <148454649+Sw-apnil@users.noreply.github.com>
2025-05-17 05:47:23 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+let Name="swapnil"
+let roll=10
+let flag=true
+let value;
+console.log(typeof Name)
+console.log(typeof "Swapnil") // dono tarike se datatype pta kr skte h
+
+// now tpes of data, not much javascript
+// >>number(int) limited capacity like int in cpp
+// >>bigint ,like long long
+// boolean>>true/false
+// null>>> standalone value
+// undefined
+// these are the datattypes. 
